2fe78102de0202d0ff8bea0b0f5843bd2145d0ca
Alefe Bispo <alefebsp@hotmail.com>
2023-06-17 14:44:33 -0300

    removed find registry by day use case

--- src/app/useCases/registry/find-registry-by-day/find-registry-by-day.ts
+++
@@ -1,8 +0,0 @@
-import { RegistriesRepository } from '@app/repositories/registries-repository';
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class FindRegistryByDay {
-  constructor(private registriesRepository: RegistriesRepository) {}
-  async execute(collaboratorId: string, day: string) {}
-}
