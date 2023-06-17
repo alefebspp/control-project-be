6256c4b48c899938f1d3a7836fa930284a20e8f2
Alefe Bispo <alefebsp@hotmail.com>
2023-06-17 14:42:35 -0300

    defined auth service, controller and module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,44 @@
+import { CollaboratorsRepository } from '@app/repositories/collaborators-repository';
 import { Injectable } from '@nestjs/common';
+import { compareSync } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { Collaborator } from '@app/entities/collaborator/collaborator';
+import { Payload } from './interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private collaboratorsRepository: CollaboratorsRepository,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, password: string) {
+    const collaborator = await this.collaboratorsRepository.findByEmail(
+      username,
+    );
+
+    if (!collaborator) {
+      return;
+    }
+
+    const isPasswordCorrect = compareSync(password, collaborator.password);
+
+    if (!isPasswordCorrect) {
+      return;
+    }
+
+    return collaborator;
+  }
+
+  async login(user: Collaborator) {
+    const payload: Payload = {
+      user_email: user.email,
+      user_id: user.id,
+      user_name: user.name,
+      user_surname: user.surname,
+    };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
