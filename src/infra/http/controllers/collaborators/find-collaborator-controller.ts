257ab105eff5bc06f80aa2a51257f9b327d998eb
Alefe Bispo <alefebsp@hotmail.com>
2023-06-17 14:46:20 -0300

    defined JwtAthGuard at find collaborator controller

--- src/infra/http/controllers/collaborators/find-collaborator-controller.ts
+++ src/infra/http/controllers/collaborators/find-collaborator-controller.ts
@@ -1,10 +1,12 @@
 import { FindCollaborator } from '@app/useCases/collaborator/find-collaborator/find-collaborator';
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('collaborator')
 export class FindCollaboratorController {
   constructor(private findCollaborator: FindCollaborator) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get('/:collaboratorId')
   async find(@Param('collaboratorId') collaboratorId: string) {
     const { collaborator } = await this.findCollaborator.execute(
