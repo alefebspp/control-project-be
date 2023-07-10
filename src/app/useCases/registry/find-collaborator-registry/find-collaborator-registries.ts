91336cd94309a36d2558a745f9703d1d6619977d
Alefe Bispo <alefebsp@hotmail.com>
2023-07-10 11:44:58 -0300

    redefined find collaborator method

--- src/app/useCases/registry/find-collaborator-registry/find-collaborator-registries.ts
+++ src/app/useCases/registry/find-collaborator-registry/find-collaborator-registries.ts
@@ -5,11 +5,16 @@ import { Injectable } from '@nestjs/common';
 export class FindCollaboratorRegistries {
   constructor(private registriesRepository: RegistriesRepository) {}
 
-  async execute(collaboratorId: string, date: string | undefined) {
+  async execute(
+    collaboratorId: string,
+    date: string | undefined,
+    period: string | undefined,
+  ) {
     const registries =
       await this.registriesRepository.findCollaboratorRegistries(
         collaboratorId,
         date,
+        period,
       );
 
     return registries;
