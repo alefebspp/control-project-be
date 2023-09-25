d45eab2af02a073992061f325b248fc6a4f6dcf1
Alefe Bispo <alefebsp@hotmail.com>
2023-09-24 23:44:57 -0300

    defined create hour record at validate adjustment

--- src/app/useCases/request/validate-adjustment/validate-adjustment.ts
+++ src/app/useCases/request/validate-adjustment/validate-adjustment.ts
@@ -1,6 +1,7 @@
 import { StatusType } from '@app/entities/adjustment/adjustment';
 import { AdjustmentsRepository } from '@app/repositories/adjustments-repository';
 import { RegistriesRepository } from '@app/repositories/registries-repository';
+import { CreateHourRecord } from '@app/useCases/hour-record/create-hour-record/create-hour-record';
 import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
@@ -8,6 +9,7 @@ export class ValidateAdjustment {
   constructor(
     private adjustmentsRepository: AdjustmentsRepository,
     private registriesRepository: RegistriesRepository,
+    private createHourRecord: CreateHourRecord,
   ) {}
 
   async execute(adjustmentId: string, reviewer: string, newStatus: StatusType) {
@@ -27,11 +29,23 @@ export class ValidateAdjustment {
         newStatus,
       });
 
+    const {
+      registry_id,
+      registry_type,
+      new_value,
+      new_location,
+      collaborator_id,
+    } = evaluatedAdjustment;
+
     if (newStatus == 'ACCEPTED') {
-      await this.registriesRepository.update(evaluatedAdjustment.registry_id, {
-        [evaluatedAdjustment.registry_type]: evaluatedAdjustment.new_value,
-        [`${evaluatedAdjustment.registry_type}_location`]:
-          evaluatedAdjustment.new_location,
+      await this.registriesRepository.update(registry_id, {
+        [registry_type]: new_value,
+        [`${registry_type}_location`]: new_location,
+      });
+
+      await this.createHourRecord.execute(collaborator_id, registry_id, {
+        registry_type,
+        new_registry: new_value,
       });
     }
 
